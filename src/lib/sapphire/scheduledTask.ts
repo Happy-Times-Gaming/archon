a760b4d10fe539a6db5a6d540c4d902dd1565733
Sean Latimer <sean@latimer.rocks>
2025-01-19 01:57:13 +0000

    feat: added daily capy and capy command

---
+++ src/lib/sapphire/scheduledTask.ts
@@ -0,0 +1,46 @@
+import type { Awaitable } from '@sapphire/framework'
+import { ScheduledTask as BaseScheduledTask, type ScheduledTasksKeys, type ScheduledTasksPayload } from '@sapphire/plugin-scheduled-tasks'
+import { withSpan } from '#lib/util/tracing'
+import { tracer } from './util'
+
+export abstract class ScheduledTask<
+  Task extends ScheduledTasksKeys = ScheduledTasksKeys,
+  Options extends ScheduledTask.Options = ScheduledTask.Options,
+> extends BaseScheduledTask<Task, Options> {
+  constructor(context: ScheduledTask.LoaderContext, options: Options) {
+    super(context, options)
+
+    this.run = this.instrumentedRun(this.run.bind(this))
+  }
+
+  private instrumentedRun(
+    fn: (payload: ScheduledTasksPayload<Task>) => Awaitable<unknown>,
+  ): (payload: ScheduledTasksPayload<Task>) => Awaitable<unknown> {
+    return async payload =>
+      withSpan(`scheduled-task.${this.name}`, async (span) => {
+        span.setAttributes({
+          'scheduled-task.name': this.name,
+          'scheduled-task.timezone': this.timezone,
+        })
+
+        if (typeof this.interval === 'number') {
+          span.setAttributes({
+            'scheduled-task.interval': this.interval,
+          })
+        }
+        if (typeof this.pattern === 'string') {
+          span.setAttributes({
+            'scheduled-task.pattern': this.pattern,
+          })
+        }
+        return fn(payload)
+      }, tracer)
+  }
+}
+
+export namespace ScheduledTask {
+  export type Options = BaseScheduledTask.Options
+  export type LoaderContext = BaseScheduledTask.LoaderContext
+  export type JSON = BaseScheduledTask.JSON
+  export type LocationJSON = BaseScheduledTask.LocationJSON
+}
---
+++ src/modules/fun/commands/capy.ts
@@ -0,0 +1,43 @@
+import type { ApplicationCommandRegistry } from '@sapphire/framework'
+import { ApplyOptions } from '@sapphire/decorators'
+import embeds from '#lib/embeds'
+import { Command } from '#lib/sapphire'
+
+@ApplyOptions<Command.Options>({
+  name: 'capy',
+  description: 'Get a random Capybara.',
+})
+export class UserCommand extends Command {
+  override registerApplicationCommands(registry: ApplicationCommandRegistry) {
+    registry.registerChatInputCommand(
+      builder =>
+        builder
+          .setName(this.name)
+          .setDescription(this.description),
+      {
+        idHints: [
+          // Archon Dev
+          '1330298570972336289',
+          // Archon Stg
+          '1330296880013250603',
+        ],
+      },
+    )
+  }
+
+  public override async chatInputRun(interaction: Command.ChatInputCommandInteraction) {
+    await interaction.deferReply()
+
+    await interaction.editReply({
+      embeds: [
+        embeds.card('Capy Roulette').setImage('attachment://capybara.jpg').setColor('DarkOrange'),
+      ],
+      files: [
+        {
+          attachment: 'https://api.capy.lol/v1/capybara',
+          name: 'capybara.jpg',
+        },
+      ],
+    })
+  }
+}
